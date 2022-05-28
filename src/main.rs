4474ad23f460b190509b0b0725790b346ea94d11
cklxx <1293822641@qq.com>
2022-05-29 01:02:39 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,12 @@ use stretch::style::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let (mut layout, mut noder) = mock_layout("foo.html");
+    let (mut layout, mut noder) = mock_layout("test/foo.html").await;
     let mut frame = 0.;
     let start = now();
     loop {
         if frame == 60. {
-            (layout, noder) = mock_layout("bar.html");
+            (layout, noder) = mock_layout("test/bar.html").await;
         }
         frame += 1.;
         layout
@@ -39,14 +39,16 @@ async fn main() {
             .unwrap();
 
         let fps = frame / (now() - start);
-        dbg!(fps);
 
-        draw_text(
+        draw_text_ex(
             &format!("FPS: {}", &fps.to_string()).to_string(),
             500.,
             500.,
-            20.0,
-            BLUE,
+            TextParams {
+                font_size: 15,
+                // font: layout.font,
+                ..Default::default()
+            },
         );
         next_frame().await
     }
@@ -59,6 +61,7 @@ struct VAttributes {
 struct Layout {
     stretch: stretch::Stretch,
     store: HashMap<Node, VAttributes>,
+    // font: Font,
 }
 
 impl Layout {
@@ -67,12 +70,13 @@ impl Layout {
         Layout {
             stretch: stretch::node::Stretch::new(),
             store,
+            // font,
         }
     }
     fn new_node(&mut self, style: Style, children: Vec<Node>) -> Result<Node, stretch::Error> {
         self.stretch.new_node(style, children)
     }
-    fn get_loc(root: stretch::result::Layout, loc: Point<f32>) -> (f32, f32) {
+    fn get_loc(root: &stretch::result::Layout, loc: Point<f32>) -> (f32, f32) {
         (root.location.x + loc.x, root.location.y + loc.y)
     }
 
@@ -97,23 +101,27 @@ impl Layout {
             1.0,
         );
 
-        let (x, y) = Layout::get_loc(*root_layout, location);
-
+        let (x, y) = Layout::get_loc(root_layout, location);
         if root_layout.size.height != 0.0 && root_layout.size.width != 0.0 {
-            dbg!(
-                x,
-                y,
-                root_layout.size.width,
-                root_layout.size.height,
-                color,
-                &text
-            );
             draw_rectangle(x, y, root_layout.size.width, root_layout.size.height, color);
         }
-        draw_text(&text, x, y + 50.0, 20.0, fcolor);
+        if text != "" {
+            draw_text_ex(
+                &text,
+                x,
+                y + 20.,
+                TextParams {
+                    font_size: 20,
+                    // font: self.font,
+                    color: fcolor,
+                    ..Default::default()
+                },
+            );
+        }
+
         if let Ok(nodes) = self.stretch.children(*root) {
             for node in nodes {
-                self.render_layout(&node, root_layout.location).unwrap();
+                self.render_layout(&node, Point { x, y }).unwrap();
             }
         }
         Ok(())
@@ -132,13 +140,13 @@ impl Layout {
             .unwrap();
 
         match node.data {
-            NodeData::Document => println!("#document"),
+            NodeData::Document => (),
 
             NodeData::Text { ref contents } => {
                 self.store.insert(
                     vnode,
                     VAttributes {
-                        text: trim_text(contents.borrow().escape_default().to_string()),
+                        text: trim_text(contents.borrow().to_string()),
                         ..Default::default()
                     },
                 );
@@ -180,9 +188,9 @@ impl Layout {
             .filter(|child| match &child.data {
                 NodeData::Text { contents } => {
                     let mut result = true;
-                    let mut text_temp = contents.borrow().escape_default().to_string();
+                    let mut text_temp: String = contents.borrow().to_string();
                     text_temp = text_temp.trim().to_string();
-                    if text_temp == "\\n" {
+                    if text_temp == "" {
                         result = false;
                     }
                     result
@@ -196,27 +204,74 @@ impl Layout {
     }
 }
 
-fn mock_layout(path: &str) -> (Layout, stretch::node::Node) {
-    let f = File::open(path).unwrap();
-    let mut f = BufReader::new(f);
+async fn mock_layout(path: &str) -> (Layout, stretch::node::Node) {
+    // let f = File::open(path).unwrap();
+    // let font = load_ttf_font("test/杨任东竹石体-Light.ttf").await.unwrap();
+    // let mut f = BufReader::new(f);
 
+    let html = r#"<body>
+	<div
+		id="box"
+		style="width: 400; height: 333; background-color: rgb(87, 214, 73);display: flex;flex-direction: column;"
+	>
+		box
+		<div
+			id="overview"
+			style="width: 100; height: 333; background-color: rgb(172, 31, 31)"
+		>
+			搜索
+		</div>
+		<div
+			id="overview"
+			style="width: 100; height: 333; background-color: rgb(20, 133, 203)"
+		>
+			搜索界面
+		</div>
+		<div
+			id="overview"
+			style="width: 200; height: 333; background-color: rgb(172, 31, 31)"
+		>
+			<div
+				id="overview"
+				style="
+					width: 100;
+					height: 333;
+					background-color: rgb(40, 82, 109);
+				"
+			>
+				hello world
+			</div>
+			<div
+				id="overview"
+				style="
+					width: 10;
+					height: 333;
+					background-color: rgb(12, 117, 182);
+				"
+			>
+				hello world2
+			</div>
+		</div>
+	</div>
+</body>
+"#;
     // To parse XML into a tree form, we need a TreeSink
     // luckily xml5ever comes with a static RC backed tree represetation.
     let dom: RcDom = parse_document(RcDom::default(), Default::default())
         .from_utf8()
-        .read_from(&mut f)
+        .read_from(&mut html.as_bytes())
         .unwrap();
 
     // Execute our visualizer on RcDom
 
     let mut layout = Layout::new();
-
+    let (w, h) = (screen_width(), screen_height());
     let root = layout
         .new_node(
             Style {
                 size: Size {
-                    width: Dimension::Points(500.0),
-                    height: Dimension::Auto,
+                    width: Dimension::Points(w),
+                    height: Dimension::Points(h),
                 },
                 ..Default::default()
             },
@@ -234,9 +289,8 @@ fn mock_layout(path: &str) -> (Layout, stretch::node::Node) {
 }
 
 fn trim_text(text: String) -> String {
-    text.replace("\\n\\t\\t", "")
-        .replace("\\n\\t", "")
-        .replace("\\t", "")
+    let x: &[_] = &['\n', '\t'];
+    text.trim_matches(x).to_string()
 }
 
 fn parse_style(style_strs: Vec<String>) -> (Style, VAttributes) {
@@ -291,7 +345,7 @@ fn parse_flex_direction(flex_direction: &str) -> FlexDirection {
     match flex_direction {
         "column" => FlexDirection::Column,
         "column-reverse" => FlexDirection::ColumnReverse,
-        "row-reverse"  => FlexDirection::RowReverse,
+        "row-reverse" => FlexDirection::RowReverse,
         "row" | _ => FlexDirection::Row,
     }
 }
